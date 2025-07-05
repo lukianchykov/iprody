d378308230d2dac3e7253b56e1ea0a22ac305c25
Andrii Lukianchykov <a.lukianchykov@voxloud.com>
2025-07-05 11:11:21 +0300

    Minor change

--- simple-http-server/src/main/java/com/iprody/HttpServer.java
+++ simple-http-server/src/main/java/com/iprody/HttpServer.java
@@ -1,4 +1,4 @@
-package org.example;
+package com.iprody;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
--- simple-http-server/src/test/java/com/iprody/AppTest.java
+++ simple-http-server/src/test/java/com/iprody/AppTest.java
@@ -1,4 +1,4 @@
-package org.example;
+package com.iprody;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
